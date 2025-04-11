9adc6de5ef5e4de2ddaf4f8c597bdd604b1f217b
IshikaSahu441 <ishika.sahu22@st.niituniversity.in>
2025-04-11 13:26:42 +0530

    port fix

--- app/server.js
+++ app/server.js
@@ -75,6 +75,6 @@ app.get('/get-profile', async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log("App listening on port 3000!");
 });
