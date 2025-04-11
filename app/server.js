54b290ec812f98383fba15f93b2e0bc96ba81fa1
IshikaSahu441 <ishika.sahu22@st.niituniversity.in>
2025-04-11 10:59:42 +0530

    mongo

--- app/server.js
+++ app/server.js
@@ -1,80 +1,78 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
-let app = express();
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+// Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
+// Serve profile picture
+app.get('/profile-picture', (req, res) => {
+  const img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  res.writeHead(200, { 'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-// use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+// MongoDB Configuration
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017";
+const databaseName = "my-db";
 
-// use when starting application as docker container
-let mongoUrlDocker = "mongodb://admin:password@mongodb";
+// POST: Update Profile
+app.post('/update-profile', async (req, res) => {
+  const userObj = req.body;
+  userObj.userid = 1;
 
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
-// "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "my-db";
-
-app.post('/update-profile', function (req, res) {
-  let userObj = req.body;
-
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
-    userObj['userid'] = 1;
-
-    let myquery = { userid: 1 };
-    let newvalues = { $set: userObj };
-
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
+  try {
+    const client = await MongoClient.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
-  });
-  // Send response
-  res.send(userObj);
+    const db = client.db(databaseName);
+    const result = await db.collection("users").updateOne(
+      { userid: 1 },
+      { $set: userObj },
+      { upsert: true }
+    );
+
+    await client.close();
+    res.send(userObj);
+  } catch (err) {
+    console.error("Error updating profile:", err);
+    res.status(500).send("Database error while updating profile.");
+  }
 });
 
-app.get('/get-profile', function (req, res) {
-  let response = {};
-  // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
-
-    let myquery = { userid: 1 };
-
-    db.collection("users").findOne(myquery, function (err, result) {
-      if (err) throw err;
-      response = result;
-      client.close();
-
-      // Send response
-      res.send(response ? response : {});
+// GET: Get Profile
+app.get('/get-profile', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-  });
+
+    const db = client.db(databaseName);
+    const result = await db.collection("users").findOne({ userid: 1 });
+
+    await client.close();
+    res.send(result || {});
+  } catch (err) {
+    console.error("Error fetching profile:", err);
+    res.status(500).send("Database error while fetching profile.");
+  }
 });
 
-app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+// Start the server
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
 });
